262ce3ca7923c3539d2de9bdf8475a2c66553f39
Gustavsonpy <gupaluski@gmail.com>
2024-06-23 18:29:51 -0300

    feat: CRUD prestador concluído

--- controllers/prestador_controller.js
+++ controllers/prestador_controller.js
@@ -1,70 +1,176 @@
-import { where } from "sequelize";
-import prestador from "../models/prestador_model.js";
+import Prestador from "../models/prestador_model.js";
+import db from "../config/database.js";
 
 async function syncDatabase() {
-    await prestador.sync();
+    await db.sync();
 }
 
 syncDatabase();
 
+//-------------------------------------------------------------------------------
+//CREATE   
 export let createPrestador = async(req, res) => {
     try{
-        const prestadorCreated = await prestador.create({
+        const prestador = await Prestador.create({
             nome_prestador: req.body.nome_prestador,
             tempo_experiencia: req.body.tempo_experiencia,
-            fk_exeperiencia: req.body.fk_exeperiencia,
-            fk_servico: req.body.fk_servico
+            fk_categoria: req.body.fk_categoria,
+            fk_servico: req.body.fk_servico,
         })
-        req.status(200).send(prestadorCreated);
+        res.status(200).send(prestador);
     }catch(e){
-        res.status(500).send("Algo deu errado com a criação do prestador!")
+        res.status(500).send("Algo deu errado com a criação do prestador!\n\n")
     }
 }
 
-export let getPrestadores = async(req, res) => {
+//-------------------------------------------------------------------------------
+//READ
+export let getPrestador = async(req, res) => {
     try{
-        const prestadores = await prestador.findAll();
-        res.send(prestadores);
+        const prestadores = await Prestador.findAll();
+        res.status(200).send(prestadores);
+    }catch(e){
+        res.status(500).send("Erro ao resgatar os prestadores!");
+        console.log("Erro:\n\n", e);
+    }
+}
+
+export let getPrestadorByName = async(req, res) => {
+    try{
+        const prestador = await Prestador.findAll({
+            where: {
+                nome_prestador: req.params.nome_prestador
+            }
+        });
+        if(prestador.length < 1) return res.status(500).send("Não existe este prestador")
+        res.status(200).send(prestador);
     }catch(e){
         console.log("Erro ao resgatar os prestadores!");
     }
 }
 
-// export let getPrestadoresBy = async(req, res) => {
-//     try{
-//         const prestadores = await prestador.findAll();
-//         res.send(prestadores);
-//     }catch(e){
-//         console.log("Erro ao resgatar os prestadores!");
-//     }
-// }
+export let getPrestadorByTempoExperiencia = async(req, res) => {
+    try{
+        const prestador = await Prestador.findAll({
+            where: {
+                tempo_experiencia: req.params.tempo_experiencia
+            }
+        });
+        if(prestador.length < 1) return res.status(500).send("Não existe prestador com essa experiência")
+        res.status(200).send(prestador);
+    }catch(e){
+        console.log("Erro ao resgatar os prestadores!");
+    }
+}
 
+export let getPrestadorByCategoria = async(req, res) => {
+    try{
+        const prestador = await Prestador.findAll({
+            where: {
+                fk_categoria: req.params.fk_categoria
+            }
+        });
+        if(prestador.length < 1) return res.status(500).send("Não existe prestador com essa categoria!")
+        res.status(200).send(prestador);
+    }catch(e){
+        console.log("Erro ao resgatar os prestadores!");
+    }
+}
+
+export let getPrestadorByServico = async(req, res) => {
+    try{
+        const prestador = await Prestador.findAll({
+            where: {
+                fk_servico: req.params.fk_servico
+            }
+        });
+        if(prestador.length < 1) return res.status(500).send("Não existe prestador com esse serviço!")
+        res.status(200).send(prestador);
+    }catch(e){
+        console.log("Erro ao resgatar os prestadores!");
+    }
+}
+
+//-------------------------------------------------------------------------------
+//UPDATE
 export let updateNamePrestador = async(req, res) =>{
     try{
-        if(prestador.findByPk(req.body.id_prestador)){
+        let prestador = await Prestador.findByPk(req.params.id_prestador);
+        if(prestador){
             prestador.nome_prestador = req.body.nome_prestador;
             await prestador.save();
+            res.status(200).send("Nome atualizado com sucesso!");
         }
         else{
-            res.send("Erro ao encontar o id do prestador");
+            res.status(500).send("Erro ao encontar o id do prestador");
         }
     }catch(e){
-        res.status(500).send("Não foi possível atualizar o nome do prestador\n", e);
+        res.status(500).send("Não foi possível atualizar o nome do prestador\n");
+        console.log("Erro:\n\n", e);
     }
 }
 
-export let updateExperienciaPrestador = async(req, res) =>{
+export let updateTempoExperiencia = async(req, res) =>{
     try{
-        if(prestador.findByPk(req.body.id_prestador)){
+        let prestador = await Prestador.findByPk(req.params.id_prestador);
+        if(prestador){
             prestador.tempo_experiencia = req.body.tempo_experiencia;
             await prestador.save();
+            res.status(200).send("Tempo de experiência atualizado com sucesso!");
         }
         else{
-            res.send("Erro ao encontar o id do prestador");
+            res.status(500).send("Erro ao encontar o id do prestador");
         }
     }catch(e){
-        res.status(500).send("Não foi possível atualizar o tempo de experiencia do prestador\n", e);
+        res.status(500).send("Não foi possível atualizar o tempo de experiência do prestador\n");
+        console.log("Erro:\n\n", e);
     }
 }
 
-//Fazer da fk_categoria e do fk_servico 
+export let updateCategoria = async(req, res) =>{
+    try{
+        let prestador = await Prestador.findByPk(req.params.id_prestador);
+        if(prestador){
+            prestador.fk_categoria = req.body.fk_categoria;
+            await prestador.save();
+            res.status(200).send("Categoria atualizado com sucesso!");
+        }
+        else{
+            res.status(500).send("Erro ao encontar o id do prestador");
+        }
+    }catch(e){
+        res.status(500).send("Não foi possível atualizar a categoria do prestador\n");
+        console.log("Erro:\n\n", e);
+    }
+}
+
+export let updateServico = async(req, res) =>{
+    try{
+        let prestador = await Prestador.findByPk(req.params.id_prestador);
+        if(prestador){
+            prestador.fk_servico = req.body.fk_servico;
+            await prestador.save();
+            res.status(200).send("Serviço atualizado com sucesso!");
+        }
+        else{
+            res.status(500).send("Erro ao encontar o id do prestador");
+        }
+    }catch(e){
+        res.status(500).send("Não foi possível atualizar o serviço do prestador\n");
+        console.log("Erro:\n\n", e);
+    }
+}
+
+//-------------------------------------------------------------------------------
+//DELETE
+export let deletePrestador = async(req, res) => {
+    try{
+        let prestador = await Prestador.findByPk(req.params.id_prestador);
+        if(prestador){
+            prestador.destroy();
+            res.status(200).send("Prestador deletado com sucesso!");
+        }
+    }catch(e){
+        res.status(500).send("Erro ao deletar serviço!");
+    }
+}
--- routes/prestador_route.js
+++ routes/prestador_route.js
@@ -1,11 +1,31 @@
 import express from "express";
-import { getPrestadores, createPrestador } from "../controllers/prestador_controller.js";
+import { 
+    createPrestador,
+    getPrestador,
+    getPrestadorByName,
+    getPrestadorByTempoExperiencia,
+    getPrestadorByCategoria,
+    getPrestadorByServico, 
+    updateNamePrestador,
+    updateTempoExperiencia,
+    updateCategoria,
+    updateServico,
+    deletePrestador } from "../controllers/prestador_controller.js";
 
 
 let prestador_route = express.Router();
 
-prestador_route.get('/prestadores', getPrestadores);
 prestador_route.post('/newPrestador', createPrestador);
+prestador_route.get('/prestadores', getPrestador);
+prestador_route.get('/nome/:nome_prestador', getPrestadorByName);
+prestador_route.get('/tempo_experiencia/:tempo_experiencia', getPrestadorByTempoExperiencia);
+prestador_route.get('/categoria/:fk_categoria', getPrestadorByCategoria);
+prestador_route.get('/servico/:fk_servico', getPrestadorByServico);
+prestador_route.put('/updateName/:id_prestador', updateNamePrestador);
+prestador_route.put('/updateTempoExperiencia/:id_prestador', updateTempoExperiencia);
+prestador_route.put('/updateCategoria/:id_prestador', updateCategoria);
+prestador_route.put('/updateServico/:id_prestador', updateServico);
+prestador_route.delete('/delete/:id_prestador', deletePrestador);
 
 
 export {prestador_route};
