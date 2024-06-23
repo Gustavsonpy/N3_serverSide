6214972eeca020fbcace5a10bcc1e6671fd94939
Gustavsonpy <gupaluski@gmail.com>
2024-06-23 17:47:38 -0300

    conf: Adicionado rota de categoria

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express, { Router } from "express";
 import { prestador_route } from "./routes/prestador_route.js";
 import { servico_route } from "./routes/servico_route.js";
+import { categoria_route } from "./routes/categoria_route.js";
 import db from "./config/database.js";
 
 const port = 3000;
@@ -16,6 +17,7 @@ try {
 
 server.use("/prestador", prestador_route);
 server.use("/servico", servico_route);
+server.use("/categoria", categoria_route);
 
 server.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`)
