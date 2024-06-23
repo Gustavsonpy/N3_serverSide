355e310d95a2196c793cdf6d7fd60d98c6ca52c9
Gustavsonpy <gupaluski@gmail.com>
2024-06-23 17:29:53 -0300

    feat: CRUD serviço concluído

--- controllers/servico_controller.js
+++ controllers/servico_controller.js
@@ -7,6 +7,8 @@ async function syncDatabase() {
 
 syncDatabase();
 
+//-------------------------------------------------------------------------------
+//CREATE   
 export let createServico = async(req, res) => {
     try{
         const servicoCreated = await Servico.create({
@@ -15,55 +17,96 @@ export let createServico = async(req, res) => {
         })
         res.status(200).send(servicoCreated);
     }catch(e){
-        // res.status(500).send("Algo deu errado com a criação do serviço!\n\n")
-        console.log("Algo deu errado com a criação do serviço!\n\n", e)
+        res.status(500).send("Algo deu errado com a criação do serviço!\n\n")
     }
 }
 
-// export let getPrestadores = async(req, res) => {
-//     try{
-//         const prestadores = await prestador.findAll();
-//         res.send(prestadores);
-//     }catch(e){
-//         console.log("Erro ao resgatar os prestadores!");
-//     }
-// }
+//-------------------------------------------------------------------------------
+//READ
+export let getServicos = async(req, res) => {
+    try{
+        const servicos = await Servico.findAll();
+        res.status(200).send(servicos);
+    }catch(e){
+        res.status(500).send("Erro ao resgatar os servicos!");
+        console.log("Erro:\n\n", e);
+    }
+}
 
-// export let getPrestadoresBy = async(req, res) => {
-//     try{
-//         const prestadores = await prestador.findAll();
-//         res.send(prestadores);
-//     }catch(e){
-//         console.log("Erro ao resgatar os prestadores!");
-//     }
-// }
+export let getServicoByName = async(req, res) => {
+    try{
+        const servicos = await Servico.findAll({
+            where: {
+                nome_servico: req.params.nome_servico
+            }
+        });
+        if(servicos.length < 1) return res.status(500).send("Não existe este tipo de serviço")
+        res.status(500).send(servicos);
+    }catch(e){
+        console.log("Erro ao resgatar os serviços!");
+    }
+}
 
-// export let updateNamePrestador = async(req, res) =>{
-//     try{
-//         if(prestador.findByPk(req.body.id_prestador)){
-//             prestador.nome_prestador = req.body.nome_prestador;
-//             await prestador.save();
-//         }
-//         else{
-//             res.send("Erro ao encontar o id do prestador");
-//         }
-//     }catch(e){
-//         res.status(500).send("Não foi possível atualizar o nome do prestador\n", e);
-//     }
-// }
+export let getServicoByValor = async(req, res) => {
+    try{
+        const servicos = await Servico.findAll({
+            where: {
+                valor_servico: req.params.valor_servico
+            }
+        });
+        if(servicos.length < 1) return res.status(500).send("Não existe este tipo de serviço")
+        res.status(500).send(servicos);
+    }catch(e){
+        console.log("Erro ao resgatar os serviços!");
+    }
+}
 
-// export let updateExperienciaPrestador = async(req, res) =>{
-//     try{
-//         if(prestador.findByPk(req.body.id_prestador)){
-//             prestador.tempo_experiencia = req.body.tempo_experiencia;
-//             await prestador.save();
-//         }
-//         else{
-//             res.send("Erro ao encontar o id do prestador");
-//         }
-//     }catch(e){
-//         res.status(500).send("Não foi possível atualizar o tempo de experiencia do prestador\n", e);
-//     }
-// }
+//-------------------------------------------------------------------------------
+//UPDATE
+export let updateNameServico = async(req, res) =>{
+    try{
+        let servico = await Servico.findByPk(req.params.id_servico);
+        if(servico){
+            servico.nome_servico = req.body.nome_servico;
+            await servico.save();
+            res.status(200).send("Nome atualizado com sucesso!");
+        }
+        else{
+            res.status(500).send("Erro ao encontar o id do serviço");
+        }
+    }catch(e){
+        res.status(500).send("Não foi possível atualizar o nome do servico\n");
+        console.log("Erro:\n\n", e);
+    }
+}
 
-//Fazer da fk_categoria e do fk_servico 
+export let updatePrecoServico = async(req, res) =>{
+    try{
+        let preco = await Servico.findByPk(req.params.id_servico);
+        if(preco){
+            preco.valor_servico = req.body.valor_servico;
+            await preco.save();
+            res.status(200).send("Valor atualizado com sucesso!");
+        }
+        else{
+            res.status(500).send("Erro ao encontar o id do serviço");
+        }
+    }catch(e){
+        res.status(500).send("Não foi possível atualizar o valor do servico\n");
+        console.log("Erro:\n\n", e);
+    }
+}
+
+//-------------------------------------------------------------------------------
+//DELETE
+export let deleteServico = async(req, res) => {
+    try{
+        let servico = await Servico.findByPk(req.params.id_servico);
+        if(servico){
+            servico.destroy();
+            res.status(200).send("Serviço deletado com sucesso!");
+        }
+    }catch(e){
+        res.status(500).send("Erro ao deletar serviço!");
+    }
+}
--- routes/servico_route.js
+++ routes/servico_route.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { createServico } from "../controllers/servico_controller.js"
+import { createServico, getServicos, getServicoByName, updateNameServico, updatePrecoServico, getServicoByValor, deleteServico } from "../controllers/servico_controller.js"
 
 const servico_route = express.Router();
 
 servico_route.post('/newServico', createServico);
+servico_route.get('/servicos', getServicos);
+servico_route.get('/nome/:nome_servico', getServicoByName);
+servico_route.get('/preco/:valor_servico', getServicoByValor);
+servico_route.put('/updateName/:id_servico', updateNameServico);
+servico_route.put('/updatePreco/:id_servico', updatePrecoServico);
+servico_route.delete('/delete/:id_servico', deleteServico);
 
 export { servico_route }
